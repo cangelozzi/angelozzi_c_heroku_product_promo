df960480190f0cecc3ee86b8feb65856e6a05972
camgelozzi <cangelozzi@gmail.com>
2019-03-16 22:15:01 -0400

    mobile first to Desktop Index page

---
+++ data.js
@@ -0,0 +1,25 @@
+const data = {
+  title: 'CodeCraft',
+  tagline: "Everything your child needs to learn computer programming the fun way! Ages 7+",
+  power: "Coding is a Super Power",
+  tools: "All you need is the internet and a computer",
+  whylearn1: {
+    learn: "Learn, Solve, Explore",
+    img: "puzzle.svg" 
+  },
+  whylearn2: {
+    learn: "Create Apps, Control Toys, Build Games",
+    img: "Walle-female.svg" 
+  },
+  whylearn3: {
+    learn: "Explore 3D Worlds, Mod Minecraft, Shape Environments",
+    img: "cube.svg" 
+  },
+  whylearn4: {
+    learn: "Learn Code Languages, handle abstract elements",
+    img: "landscape.svg" 
+  },
+  about: "Codecraft is a Kids Coding Platform to help children learning to code. Codecraft offers self-paced online courses for kids to learn coding at home, as well as an engaging programming curriculum for schools and camps."
+};
+
+module.exports = data;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+//! Require data.js
+const data = require('../data');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', data);
+});
+
+/* GET Learn More page. */
+router.get('/learnmore', function (req, res, next) {
+  res.render('learnmore', { title: 'Express', message: "This is an Express App!" });
 });
 
 module.exports = router;
