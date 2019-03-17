c20cf7b5fa1ee9d6ca16485aaaf5bba9491ae5cd
camgelozzi <cangelozzi@gmail.com>
2019-03-17 00:25:24 -0400

    mobile first to Desktop Learn More page

--- data.js
+++ data.js
@@ -19,7 +19,9 @@ const data = {
     learn: "Learn Code Languages, handle abstract elements",
     img: "landscape.svg" 
   },
-  about: "Codecraft is a Kids Coding Platform to help children learning to code. Codecraft offers self-paced online courses for kids to learn coding at home, as well as an engaging programming curriculum for schools and camps."
+  about: "Codecraft is a Kids Coding Platform to help children learning to code. Codecraft offers self-paced online courses for kids to learn coding at home, as well as an engaging programming curriculum for schools and camps.",
+  introtocoding: "There’s been a lot of discussion around coding lately, but it can be hard to figureout exactly what it means to code and how it plays a role in your child’s future. Coding(or computer programming), is the process of providing instructions to a computer so it performs a specific task.You may have heard of popular text languages like Java, Python, or Ruby, but even kids can easily learn to code using a visual block language like Codecraft! => Start.Walk.Jump",
+  why: "Our award-winning creative computing platform helps kids develop computational thinking and programming skills in a fun, intuitive, and imaginative way.As they’re guided through interactive game- based courses, kids quickly learn fundamental programming concepts.With Your child can apply their coding skills as they build games, tell stories, mod Minecraft, create apps, control drones and robots, and more! We even offer a parent dashboard where you can follow your child’s success and share their creations."
 };
 
 module.exports = data;
